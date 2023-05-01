0e1facae025e4a01112b7eb1470f8e754f8989f8
erick <erick.kamau@azubiafrica.org>
2023-05-01 16:08:09 +0300

    Initial commit

---
+++ quiz.js
@@ -0,0 +1,241 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+
+  {
+    numb: 6,
+    question: "What kind of government dominated most of Europe during the 17th century?",
+    answer: "Absolute Monarchy",
+    options: [
+      "Democracy",
+      "Constitutional Monarchy",
+      "Parliamentary Democracy",
+      "Absolute Monarchy"
+    ],
+  },
+  {
+    numb: 7,
+    question:  "Which philosopher famously said 'Religion is the sign of the oppressed ... it is the opium of the people'?",
+    answer: "Karl Marx",
+    options: [
+      "Ludwig Wittgenstein",
+      "Karl Marx",
+      "Socrates",
+      "Friedrich Nietzsche"
+    ],
+  },
+  {
+    numb: 8,
+    question: "Which actor played the role of Frank Booth in Blue Velvet?",
+    answer: "Dennis Hopper",
+    options: [
+      "Dennis Hopper",
+      "Kevin Spacey",
+      "James Stewart",
+      "Robert Mitchum"
+    ],
+  },
+  {
+    numb: 9,
+    question: "What is Thermology the study of?",
+    answer: "heat",
+    options: [
+      "the future",
+      "the future",
+      "heat",
+      "a neologism for the Pilates method, a system of physical exercise"
+    ],
+},
+  
+];
+
+
+
+[
+    
+    
+    {
+      "category": "arts_and_literature",
+      "id": "63afff982d1dcedfbb654568",
+      "correctAnswer": "The Tempest",
+      "incorrectAnswers": [
+        "Othello",
+        "A Midsummer Night's Dream",
+        "Romeo and Juliet"
+      ],
+      "question": {
+        "text": "In which of Shakespeare's plays does the character 'Prospero' appear?"
+      },
+      "tags": [
+        "arts_and_literature",
+        "shakespeare",
+        "theater",
+        "fictitious_characters"
+      ],
+      "type": "text_choice",
+      "difficulty": "hard",
+      "regions": [],
+      "isNiche": false
+    },
+    {
+      "category": "music",
+      "id": "622a1c387cc59eab6f950b46",
+      "correctAnswer": "The Velvet Underground",
+      "incorrectAnswers": [
+        "Alice in Chains",
+        "Marilyn Manson",
+        "Fall Out Boy"
+      ],
+      "question": {
+        "text": "Which band includes 'Nico'?"
+      },
+      "tags": [
+        "music"
+      ],
+      "type": "text_choice",
+      "difficulty": "hard",
+      "regions": [],
+      "isNiche": false
+    },
+    {
+      "category": "food_and_drink",
+      "id": "622a1c367cc59eab6f9502dc",
+      "correctAnswer": "Gin ",
+      "incorrectAnswers": [
+        "Rum",
+        "Vodka",
+        "Avocaat"
+      ],
+      "question": {
+        "text": "The Term 'Dutch Courage' Originally Came From The Consumption Of Which Drinkt?"
+      },
+      "tags": [
+        "food_and_drink"
+      ],
+      "type": "text_choice",
+      "difficulty": "medium",
+      "regions": [],
+      "isNiche": false
+    },
+    {
+      "category": "history",
+      "id": "639ae2bb929b90846f2fc8b1",
+      "correctAnswer": "Harald Hardrada",
+      "incorrectAnswers": [
+        "Ragnar Lothbrok",
+        "Ivar the Boneless",
+        "Sweyn Forkbeard"
+      ],
+      "question": {
+        "text": "What is the name of the legendary Viking warrior who fought in the Battle of Stamford Bridge?"
+      },
+      "tags": [
+        "history",
+        "vikings",
+        "uk"
+      ],
+      "type": "text_choice",
+      "difficulty": "medium",
+      "regions": [],
+      "isNiche": false
+    },
+    {
+      "category": "society_and_culture",
+      "id": "622a1c3b7cc59eab6f951712",
+      "correctAnswer": "Pantomime",
+      "incorrectAnswers": [
+        "Colosseum",
+        "Theatrical",
+        "Cabaret"
+      ],
+      "question": {
+        "text": "Which word comes from a Greek word meaning 'imitator of all'?"
+      },
+      "tags": [
+        "society_and_culture",
+        "words",
+        "language"
+      ],
+      "type": "text_choice",
+      "difficulty": "hard",
+      "regions": [],
+      "isNiche": false
+    },
+    {
+      "category": "society_and_culture",
+      "id": "622a1c3a7cc59eab6f9511b1",
+      "correctAnswer": "Eris",
+      "incorrectAnswers": [
+        "Razr",
+        "Mercury",
+        "Zeus"
+      ],
+      "question": {
+        "text": "Who is the Greek equivalent of the Roman god Discordia?"
+      },
+      "tags": [
+        "ancient_greece",
+        "mythology",
+        "society_and_culture"
+      ],
+      "type": "text_choice",
+      "difficulty": "hard",
+      "regions": [],
+      "isNiche": false
+    },
+    
+  ]
